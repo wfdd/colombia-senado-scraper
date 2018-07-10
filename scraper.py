0278cdd229be59ed0febb8f87fc6882897eb1d16
wfdd <erdbanfed@gmail.com>
2018-07-10 13:55:41 +0300

    Update

--- scraper.py
+++ scraper.py
@@ -1,31 +1,22 @@
 
 import ast
 import asyncio
-import inspect
 import re
 import sqlite3
 import sys
-from textwrap import TextWrapper
-from urllib.parse import urljoin, quote as urlquote
 
-import aiohttp
-from lxml.html import (document_fromstring as parse_html,
-                       tostring as unparse_html)
+from aiohttp import ClientSession, TCPConnector
+from lxml.html import document_fromstring, tostring
 import uvloop
+from yarl import URL
 
-base_url = 'http://www.secretariasenado.gov.co/'
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+base_url = URL('http://www.secretariasenado.gov.co/')
 
 numeric_escape_match = re.compile(r'&#(\d+);')
 email_match = re.compile(r'\n var addy\d+ = (.*);\n addy\d+ = addy\d+ \+ (.*);')
-
-loop = uvloop.new_event_loop()
-
-
-def _log(message, _wrap=TextWrapper(break_long_words=False, break_on_hyphens=False,
-                                    width=68).wrap):
-    print('\n'.join((inspect.stack()[1].function,
-                     *('  ' + m for m in _wrap(message)))
-                    ), file=sys.stderr)
 
 
 def deobfuscate_email(matches):
@@ -37,40 +28,34 @@ def deobfuscate_email(matches):
     return email
 
 
-async def scrape_person(session, semaphore, params):
+async def scrape_person(session, params):
     def extract_emails():
-        emails = unparse_html(source).decode()
-        emails = {deobfuscate_email(m.groups())
-                  for m in email_match.finditer(emails)}
+        emails = tostring(source).decode()
+        emails = {deobfuscate_email(m.groups()) for m in email_match.finditer(emails)}
         if not emails:
-            _log("Couldn't find email in " + profile_resp.url)
+            print(f'Could not find email in {profile_resp.url}', file=sys.stderr)
             return
         return ';'.join(sorted(emails,
                                key=lambda i: chr(0) if 'senado.gov.co' in i else i))
 
-    async def extract_photo():
+    def extract_photo():
         try:
             photo, = source.xpath('.//img[1]/@src')
         except ValueError:
             return
-        async with semaphore, \
-                session.head(urljoin(base_url, urlquote(photo))) as photo_resp:
-            if photo_resp.status == 200:
-                return photo_resp.url
-        _log('Discarding {} in {}.  Received error code {}'
-             .format(photo_resp.url, profile_resp.url, photo_resp.status))
+        return str(base_url.with_path(photo))
 
     def extract_other_item(caption, link=False):
         if isinstance(caption, tuple):
             return next(filter(None, (extract_other_item(i, link) for i in caption)),
                         [] if link else None)
+
         if link:
-            val = source.xpath('''\
-.//td[contains(string(.), "{}")]/following-sibling::td//a/@href'''.format(caption))
+            val = source.xpath(f'''\
+.//td[contains(string(.), "{caption}")]/following-sibling::td//a/@href''')
         else:
-            val = source.xpath('''\
-string(.//td[contains(string(.), "{}")]/following-sibling::td)'''.format(
-                caption)).strip()
+            val = source.xpath(f'''\
+string(.//td[contains(string(.), "{caption}")]/following-sibling::td)''').strip()
             if not val or val.lower() in {'no tine', 'no tiene'}:
                 return
         return val
@@ -81,19 +66,15 @@ string(.//td[contains(string(.), "{}")]/following-sibling::td)'''.format(
         if not facebook:
             return
 
-        if facebook.startswith(('facebook.com', '/facebook.com')):
-            facebook = 'https://www.' + facebook.lstrip('/')
-        elif facebook.startswith('http://social.facebook.com/'):
-            facebook = facebook.replace('http://social.facebook.com/',
-                                        'https://www.facebook.com/')
-        elif not facebook.startswith('http'):
-            facebook = urljoin('https://www.facebook.com/', facebook)
-        return facebook
+        facebook = URL('https://' + facebook.lstrip('/')
+                       if not facebook.startswith('http') else facebook)
+        return str(URL('https://www.facebook.com/').with_path(facebook.path))
 
     def extract_twitter():
         twitter = extract_other_item('TWITTER:')
         if twitter:
-            twitter = twitter.replace('https://twitter.com/', '').lstrip('@')
+            twitter = ';'.join(t.replace('https://twitter.com/', '').lstrip('@')
+                               for t in twitter.split(' - '))
         return twitter
 
     def extract_website():
@@ -106,54 +87,51 @@ string(.//td[contains(string(.), "{}")]/following-sibling::td)'''.format(
                                 ).rstrip(',') for w in (website or '').splitlines())
 
         if 'correo electrónico' in website:
-            _log('Email found in {}; skipping'.format(profile_resp.url))
+            print(f'Email found in {profile_resp.url}; skipping', file=sys.stderr)
             return
         return website or None
 
-    async with semaphore, session.get(base_url, params=sorted(params.items())) \
+    async with session.get(base_url, params=sorted(params.items())) \
             as profile_resp:
-        source, = (parse_html(await profile_resp.text())
+        source, = (document_fromstring(await profile_resp.text())
                    .xpath('//div[@class = "art-article"]'))
-    return (params['id'],
-            source.text_content().strip().splitlines()[0].strip(),
-            (await extract_photo()),
-            extract_other_item('FILIACIÓN POLÍTICA:'),
-            '2014',
-            extract_emails(),
-            extract_website(),
-            extract_other_item('TELÉFONO:'),
-            extract_facebook(),
-            extract_twitter(),
-            extract_other_item('LUGAR DE NACIMIENTO:'),
-            profile_resp.url)
+    return lambda: (params['id'],
+                    source.text_content().strip().splitlines()[0].strip(),
+                    extract_photo(),
+                    extract_other_item(('FILIACIÓN POLÍTICA:', 'FILIACION POLITICA:',
+                                        'FILIACIÓNPOLÍTICA:')),
+                    '2014',
+                    extract_emails(),
+                    extract_website(),
+                    extract_other_item('TELÉFONO:'),
+                    extract_facebook(),
+                    extract_twitter(),
+                    extract_other_item('LUGAR DE NACIMIENTO:'),
+                    str(profile_resp.url))
 
 
-async def gather_people(session, semaphore):
-    async with session.get(base_url + 'index.php/buscar-senador') as resp:
-        source = parse_html(await resp.text())
-    base_params = dict(i.xpath('./@name | ./@value')
-                       for i in source.xpath('//form[@name = "ddaForm"]'
-                                             '/input[@type = "hidden"]'))
-    people_ids = source.xpath('//form[@name = "ddaForm"]/select[@name = "id"]'
-                              '/option[position() > 1]/@value')
-    people = await asyncio.gather(*(scrape_person(session, semaphore,
-                                                  {**base_params, 'id': i})
-                                    for i in people_ids), loop=loop)
-    return people
+async def gather_people():
+    async with ClientSession(connector=TCPConnector(limit_per_host=8)) \
+            as session:
+        async with session.get(base_url / 'index.php/buscar-senador') as resp:
+            source = document_fromstring(await resp.text())
+        base_params = dict(i.xpath('./@name | ./@value') for i in source.xpath('''\
+//form[@name = "ddaForm"]/input[@type = "hidden"]'''))
+        return await asyncio.gather(*(scrape_person(session, {**base_params, 'id': i})
+                                      for i in source.xpath('''\
+//form[@name = "ddaForm"]/select[@name = "id"]/option[position() > 1]/@value''')))
 
 
 def main():
-    with aiohttp.ClientSession(loop=loop) as session, \
-            sqlite3.connect('data.sqlite') as cursor:
-        people = loop.run_until_complete(gather_people(
-            session, asyncio.Semaphore(10, loop=loop)))
+    people = asyncio.run(gather_people())
+    with sqlite3.connect('data.sqlite') as cursor:
         cursor.execute('''\
 CREATE TABLE IF NOT EXISTS data
 (id, name, image, 'group', term, email, website, phone, facebook, twitter,
  place_of_birth, source, UNIQUE (id))''')
         cursor.executemany('''\
 INSERT OR REPLACE INTO data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-            people)
+            (p() for p in people))
 
 if __name__ == '__main__':
     main()
